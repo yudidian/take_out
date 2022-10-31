b25aca457a6689915f863f937d340665b1106583
denghongyu <1848761082@qq.com>
2022-10-31 17:38:59 +0800

    Squashed commit of the following:
    commit 63286c654a91d3e12c1ffdd96bdd7fe3e62ffb10
    Author: denghongyu <1848761082@qq.com>
    Date:   Mon Oct 31 17:31:57 2022 +0800
    
        feat: 添加Redis

--- src/test/java/com/dhy/TakeOutApplicationTests.java
+++ src/test/java/com/dhy/TakeOutApplicationTests.java
@@ -1,13 +1,26 @@
 package com.dhy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+
+import java.util.Set;
 
 @SpringBootTest
 class TakeOutApplicationTests {
 
+  @Autowired
+  private RedisTemplate redisTemplate;
   @Test
   void contextLoads() {
+    SetOperations setOperations = redisTemplate.opsForSet();
+    setOperations.add("MySet", "a", "b", "c");
+    Set<String> mySet = setOperations.members("MySet");
+    for (String key: mySet) {
+      System.out.println(key);
+    }
   }
 
 }
