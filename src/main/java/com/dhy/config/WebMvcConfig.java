1076146c460340cba20daf7e095a0d1c45c3efea
denghongyu <1848761082@qq.com>
2022-11-04 00:39:21 +0800

    fix: 添加swagger，测试发布

--- src/main/java/com/dhy/config/WebMvcConfig.java
+++ src/main/java/com/dhy/config/WebMvcConfig.java
@@ -1,17 +1,103 @@
 package com.dhy.config;
 
+import com.dhy.common.JacksonObjectMapper;
+import com.dhy.interceptor.TokenInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+@EnableSwagger2
+@EnableKnife4j
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedHeaders("*")
+        .allowedMethods("*")
+        .maxAge(1800)
+        .allowedOrigins("*");
+  }
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new TokenInterceptor())
+        .addPathPatterns("/**")
+        .excludePathPatterns("/employee/login")
+        .excludePathPatterns("/download")
+        .excludePathPatterns("/email/**")
+        .excludePathPatterns("/user/login")
+        .excludePathPatterns("/koala")
+        .excludePathPatterns("/doc.html")
+        .excludePathPatterns("/webjars/**")
+        .excludePathPatterns("/swagger-resources")
+        .excludePathPatterns("/v2/api-docs");
+  }
+
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converterList) {
+    MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+    messageConverter.setObjectMapper(new JacksonObjectMapper());
+    converterList.add(0, messageConverter);
+  }
+
   /**
-   * 设置静态资源
+   * 添加消息转化类
+   *
+   * @param list
    */
   @Override
-  protected  void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
-    registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+  public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+    MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+    ObjectMapper objectMapper = jsonConverter.getObjectMapper();
+    //序列换成json时,将所有的long变成string
+    SimpleModule simpleModule = new SimpleModule();
+    simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+    simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+    objectMapper.registerModule(simpleModule);
+    list.add(jsonConverter);
+  }
+
+  /**
+   * 这个方法是用来配置静态资源的，比如html，js，css，等等
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    registry.addResourceHandler("/doc.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+
+  @Bean
+  public Docket createRestApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.dhy.controller"))
+        .paths(PathSelectors.any())
+        .build();
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("koala")
+        .version("1.0.0")
+        .description("koala外卖接口文档")
+        .build();
   }
 }
