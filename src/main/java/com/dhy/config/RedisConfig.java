320dab0c3dbca04d6edd4fb16fa0ca3fb7b6d460
denghongyu <1848761082@qq.com>
2022-11-08 18:54:07 +0800

    fix: 修改Redis序列化方式

--- src/main/java/com/dhy/config/RedisConfig.java
+++ src/main/java/com/dhy/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -12,12 +13,17 @@ public class RedisConfig{
     /**
      * redisTemplate相关
      */
-    @Bean("customRedisTemplate")
-    @Primary
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(factory);
+    @Bean()
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionfactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setConnectionFactory(redisConnectionfactory);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }
