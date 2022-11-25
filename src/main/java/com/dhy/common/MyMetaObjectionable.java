88024ad470556c64b1d200234e070d60d7ee6d8f
denghongyu <1848761082@qq.com>
2022-11-25 18:27:49 +0800

    fix: 修改时间格式引起问题

--- src/main/java/com/dhy/common/MyMetaObjectionable.java
+++ src/main/java/com/dhy/common/MyMetaObjectionable.java
@@ -4,22 +4,21 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Component
 public class MyMetaObjectionable implements MetaObjectHandler {
   @Override
   public void insertFill(MetaObject metaObject) {
-    metaObject.setValue("createTime", LocalDateTime.now());
-    metaObject.setValue("updateTime", LocalDateTime.now());
+    metaObject.setValue("createTime", new Date());
+    metaObject.setValue("updateTime", new Date());
     metaObject.setValue("createUser", BaseContext.getCurrentId());
     metaObject.setValue("updateUser", BaseContext.getCurrentId());
   }
 
   @Override
   public void updateFill(MetaObject metaObject) {
-    metaObject.setValue("updateTime", LocalDateTime.now());
+    metaObject.setValue("updateTime", new Date());
     metaObject.setValue("updateUser", BaseContext.getCurrentId());
   }
 }
